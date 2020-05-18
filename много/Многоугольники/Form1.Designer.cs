c357529a4549ec4d7fe2d897318583f9fd8c3bc5
Zaamby <32413350+Zaamby@users.noreply.github.com>
2020-05-18 15:26:41 +0300

    Add files via upload

---
+++ много/Многоугольники/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Многоугольники
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.shapeToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.circleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.squareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.triangleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.borderColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.radiusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(850, 421);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove_1);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.shapeToolStripMenuItem1,
+            this.settingsToolStripMenuItem,
+            this.fileToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(850, 33);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // shapeToolStripMenuItem1
+            // 
+            this.shapeToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.circleToolStripMenuItem,
+            this.squareToolStripMenuItem,
+            this.triangleToolStripMenuItem});
+            this.shapeToolStripMenuItem1.Name = "shapeToolStripMenuItem1";
+            this.shapeToolStripMenuItem1.Size = new System.Drawing.Size(73, 29);
+            this.shapeToolStripMenuItem1.Text = "Shape";
+            // 
+            // circleToolStripMenuItem
+            // 
+            this.circleToolStripMenuItem.Name = "circleToolStripMenuItem";
+            this.circleToolStripMenuItem.Size = new System.Drawing.Size(157, 30);
+            this.circleToolStripMenuItem.Text = "Circle";
+            this.circleToolStripMenuItem.Click += new System.EventHandler(this.circleToolStripMenuItem_Click);
+            // 
+            // squareToolStripMenuItem
+            // 
+            this.squareToolStripMenuItem.Name = "squareToolStripMenuItem";
+            this.squareToolStripMenuItem.Size = new System.Drawing.Size(157, 30);
+            this.squareToolStripMenuItem.Text = "Square";
+            this.squareToolStripMenuItem.Click += new System.EventHandler(this.squareToolStripMenuItem_Click);
+            // 
+            // triangleToolStripMenuItem
+            // 
+            this.triangleToolStripMenuItem.Name = "triangleToolStripMenuItem";
+            this.triangleToolStripMenuItem.Size = new System.Drawing.Size(157, 30);
+            this.triangleToolStripMenuItem.Text = "Triangle";
+            this.triangleToolStripMenuItem.Click += new System.EventHandler(this.triangleToolStripMenuItem_Click);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colorToolStripMenuItem,
+            this.borderColorToolStripMenuItem,
+            this.radiusToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(88, 29);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(198, 30);
+            this.colorToolStripMenuItem.Text = "Shape Color";
+            this.colorToolStripMenuItem.Click += new System.EventHandler(this.colorToolStripMenuItem_Click);
+            // 
+            // borderColorToolStripMenuItem
+            // 
+            this.borderColorToolStripMenuItem.Name = "borderColorToolStripMenuItem";
+            this.borderColorToolStripMenuItem.Size = new System.Drawing.Size(198, 30);
+            this.borderColorToolStripMenuItem.Text = "Border Color";
+            this.borderColorToolStripMenuItem.Click += new System.EventHandler(this.borderColorToolStripMenuItem_Click);
+            // 
+            // radiusToolStripMenuItem
+            // 
+            this.radiusToolStripMenuItem.Name = "radiusToolStripMenuItem";
+            this.radiusToolStripMenuItem.Size = new System.Drawing.Size(198, 30);
+            this.radiusToolStripMenuItem.Text = "Radius";
+            this.radiusToolStripMenuItem.Click += new System.EventHandler(this.radiusToolStripMenuItem_Click);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(141, 30);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(141, 30);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(40, 29);
+            this.toolStripMenuItem1.Text = "►";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(40, 29);
+            this.toolStripMenuItem2.Text = "■";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(40, 29);
+            this.toolStripMenuItem3.Text = "←";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(40, 29);
+            this.toolStripMenuItem4.Text = "→";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 454);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Polygons";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem shapeToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem circleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem squareToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem triangleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem radiusToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ToolStripMenuItem borderColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+    }
+}
+
---
+++ много/Многоугольники/Form1.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Многоугольники
+{
+    public partial class Form1 : Form
+    {
+        List<Shape> tochka = new List<Shape>();
+        Graphics g;
+        int rx, ry, r;
+        int del = -1;
+        bool flag = false;
+        TrackBar radi;
+        Random rnd = new Random();
+        Form rad;
+        Type[] t = new Type[3];
+        XmlSerializer formatter;
+        UndoRedo undoredo;
+        Color c, cout;
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+            Shape strt = new Circle(Size.Width / 2, Size.Height / 2);
+            tochka.Add(strt);
+            pictureBox1.Invalidate();
+            circleToolStripMenuItem.Checked = true;
+            t[0] = typeof(List<Circle>);
+            t[1] = typeof(List<Square>);
+            t[2] = typeof(List<Triangle>);
+            formatter = new XmlSerializer(typeof(List<Shape>), t );
+            saveFileDialog1.AddExtension = true;
+            saveFileDialog1.DefaultExt = ".mng";
+            openFileDialog1.DefaultExt = ".mng";
+            openFileDialog1.Filter = "Многоугольники(*.mng)|*.mng";
+            undoredo = new UndoRedo(5);
+            undoredo.AddAction(tochka,Shape.Getc,Shape.Getcout,Shape.Getr);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (flag == false)
+            {
+                for (int i = 0; i < tochka.Count; i++)
+                {
+                    if (tochka[i].Popali(e.X, e.Y) == true) { flag = true; tochka[i].Getris = true; rx = e.X; ry = e.Y; }
+                }
+            }
+            if (flag == false && tochka.Count > 3 && Shape.Popal(tochka, e.Location) == true)
+            {
+                flag = true;
+                for (int i = 0; i < tochka.Count; i++)
+                {
+                    tochka[i].Getris = true; rx = e.X; ry = e.Y;
+                }
+            }
+            if (flag == false)
+            {
+                if (circleToolStripMenuItem.Checked == true)
+                {
+                    tochka.Add(new Circle(e.X, e.Y));
+                    pictureBox1.Invalidate();
+                }
+                else
+                {
+                    if (squareToolStripMenuItem.Checked == true)
+                    {
+                        tochka.Add(new Square(e.X, e.Y));
+                        pictureBox1.Invalidate();
+                    }
+                    else
+                    {
+                        if (triangleToolStripMenuItem.Checked == true)
+                        {
+                            tochka.Add(new Triangle(e.X, e.Y));
+                            pictureBox1.Invalidate();
+                        }
+                    }
+                }
+                for (int i = 0; i < tochka.Count; i++)
+                {
+                    if (tochka[i].Popali(e.X, e.Y) == true) { flag = true; tochka[i].Getris = true; rx = e.X; ry = e.Y; }
+                }
+            }
+
+        }
+
+        private void shapeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right && tochka.Count > 0)
+            {
+                for (int i = 0; i < tochka.Count; i++)
+                {
+                    if (tochka[i].Popali(e.X, e.Y) == true) del = i;
+                }
+                if (del >= 0) tochka.RemoveAt(del);
+                del = -1;
+                pictureBox1.Invalidate();
+                undoredo.AddAction(tochka,Shape.Getc,Shape.Getcout, Shape.Getr);
+            }
+           
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            for (int i = 0; i < tochka.Count; i++)
+            {
+                tochka[i].Getx += rnd.Next(-1, 2);
+                tochka[i].Gety += rnd.Next(-1, 2);
+            }
+            pictureBox1.Invalidate();
+
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            flag = false;
+            foreach (Shape a in tochka)
+            {
+                a.Getris = false;
+            }
+            if (tochka.Count > 3)
+            {
+                tochka = Shape.Obolochka(tochka);
+            }
+            undoredo.AddAction(tochka, Shape.Getc, Shape.Getcout, Shape.Getr);
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            g.Clear(Color.White);
+            if (tochka.Count > 3)
+            {
+                for (int i = 0; i < tochka.Count; i++)
+                {
+                    if (i < tochka.Count - 1)
+                        g.DrawLine(new Pen(Color.Black), tochka[i].Getx, tochka[i].Gety, tochka[i + 1].Getx, tochka[i + 1].Gety);
+                    else g.DrawLine(new Pen(Color.Black), tochka[i].Getx, tochka[i].Gety, tochka[0].Getx, tochka[0].Gety);
+                }
+            }
+            foreach (Shape a in tochka)
+            {
+                a.Draw(g);
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            g = pictureBox1.CreateGraphics();
+            pictureBox1.Invalidate();
+        }
+
+        private void circleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            circleToolStripMenuItem.Checked = true;
+            squareToolStripMenuItem.Checked = false;
+            triangleToolStripMenuItem.Checked = false;
+        }
+
+        private void squareToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            circleToolStripMenuItem.Checked = false;
+            squareToolStripMenuItem.Checked = true;
+            triangleToolStripMenuItem.Checked = false;
+        }
+
+        private void triangleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            circleToolStripMenuItem.Checked = false;
+            squareToolStripMenuItem.Checked = false;
+            triangleToolStripMenuItem.Checked = true;
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            Shape.Getc = colorDialog1.Color;
+            undoredo.AddAction(tochka, Shape.Getc, Shape.Getcout, Shape.Getr);
+        }
+
+        private void borderColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            Shape.Getcout = colorDialog1.Color;
+            undoredo.AddAction(tochka, Shape.Getc, Shape.Getcout, Shape.Getr);
+        }
+
+        private void radiusToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rad = new Form();
+            rad.Owner = this;
+            rad.ShowIcon = false;
+            rad.ShowInTaskbar = false;
+            rad.MinimizeBox = false;
+            rad.MaximizeBox = false;
+            rad.SizeGripStyle = SizeGripStyle.Hide;
+            Size l = new Size(200, 100);
+            rad.MaximumSize = l;
+            rad.MinimumSize = l;
+            rad.Size = l;
+            radi = new TrackBar();
+            radi.Minimum = 5;
+            radi.Maximum = 25;
+            radi.Value = 10;
+            l = new Size(100, 25);
+            radi.Size = l;
+            radi.Parent = rad;
+            radi.Minimum = 5;
+            radi.Maximum = 25;
+            radi.Value = 10;
+            radi.Location = new Point(40, 8);
+            rad.Show();
+            radi.Scroll += new EventHandler(this.rad_Scroll);
+            rad.Disposed += new EventHandler(this.radiusToolStripMenuItem_Disposed);
+        }
+        private void radiusToolStripMenuItem_Disposed(object sender, EventArgs e)
+        {
+            undoredo.AddAction(tochka, Shape.Getc, Shape.Getcout, Shape.Getr);
+        }
+        private void rad_Scroll(object sender, EventArgs e)
+        {
+            Shape.Getr = radi.Value;
+            pictureBox1.Invalidate();
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            if (tochka.Count > 3)
+            {
+                tochka = Shape.Obolochka(tochka);
+            }
+            pictureBox1.Invalidate();
+            undoredo.AddAction(tochka, Shape.Getc, Shape.Getcout, Shape.Getr);
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            using (TextReader textReader = new StreamReader(openFileDialog1.FileName))
+            {
+                tochka = (List<Shape>)formatter.Deserialize(textReader);
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            using (TextWriter textWriter = new StreamWriter(saveFileDialog1.FileName))
+            {
+                formatter.Serialize(textWriter, tochka);
+            }
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            tochka = undoredo.Undo(out c, out cout, out r);
+            List<Shape> toc = new List<Shape>();
+
+            Shape l = new Circle();
+            foreach (Shape a in tochka)
+            {
+                if (a is Circle)
+                { l = new Circle(a.Getx, a.Gety); }
+                if (a is Square) l = new Square(a.Getx, a.Gety);
+                if (a is Triangle) l = new Triangle(a.Getx, a.Gety);
+                toc.Add(l);
+            }
+            tochka = toc;
+            Shape.Getc = c;
+            Shape.Getcout = cout;
+            Shape.Getr = r;
+            pictureBox1.Invalidate();
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            tochka = undoredo.Redo(out c, out cout, out r);
+            List<Shape> toc = new List<Shape>();
+            Shape l = new Circle();
+            foreach (Shape a in tochka)
+            {
+                if (a is Circle)
+                { l = new Circle(a.Getx, a.Gety); }
+                if (a is Square) l = new Square(a.Getx, a.Gety);
+                if (a is Triangle) l = new Triangle(a.Getx, a.Gety);
+                toc.Add(l);
+            }
+            tochka = toc;
+            Shape.Getc = c;
+            Shape.Getcout = cout;
+            Shape.Getr = r;
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_MouseMove_1(object sender, MouseEventArgs e)
+        {
+
+            if (flag == true)
+            {
+
+                foreach (Shape a in tochka)
+                {
+                    if (a.Getris == true)
+                    {
+                        a.Getx += e.X - rx;
+                        a.Gety += e.Y - ry;
+                    }
+                }
+                pictureBox1.Invalidate();
+                rx = e.X; ry = e.Y;
+            }
+            pictureBox1.Invalidate();
+        }
+    }
+}
